6da9454ba428ffe7bdc9585ba603ee4fd69b6bd7
Diego Martinez <juandiego337@hotmail.com>
2021-09-23 22:31:55 -0500

    TenniGame3 is refactored

--- src/main/java/sofka/com/tennis/TennisGame3.java
+++ src/main/java/sofka/com/tennis/TennisGame3.java
@@ -1,37 +1,67 @@
 package sofka.com.tennis;
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private int scorePlayerOne;
+    private int scorePlayerTwo;
+
+    private final String namePlayerOne;
+    private final String namePlayerTwo;
+
+
+    public TennisGame3(String puntNam1, String puntNan2) {
+        this.namePlayerOne = puntNam1;
+        this.namePlayerTwo = puntNan2;
     }
+
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        if (scores()) {
+            return scoreResult();
+        }
+
+        if (isTie()) {
+            return "Deuce";
+        }
+        
+        return getWinner();
+    }
+
+    public void wonPoint(String playerName) {
+        if(playerName.equals("player1")){
+            scorePlayerTwo++;
+        }
+        if(playerName.equals("player2")){
+            scorePlayerOne++;
         }
     }
-    
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
-        else
-            this.p2 += 1;
-        
+
+    public boolean scores(){
+        return (scorePlayerTwo < 4) && (scorePlayerOne < 4) && (scorePlayerTwo + scorePlayerOne != 6);
     }
+
+    public boolean isTie(){
+        return scorePlayerTwo == scorePlayerOne;
+    }
+
+    public String getWinner(){
+        return ( raiseSubtraction() == 1) ? "Advantage " + playerForward() : "Win for " + playerForward();
+    }
+
+    public double raiseSubtraction(){
+        return Math.pow((scorePlayerTwo - scorePlayerOne), 2);
+    }
+
+    public String playerForward(){
+        return scorePlayerTwo > scorePlayerOne ? namePlayerOne : namePlayerTwo;
+    }
+
+    public String scoreResult(){
+        String[] scores = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        return (scorePlayerTwo == scorePlayerOne) ?
+                scores[scorePlayerTwo] + "-All" :
+                scores[scorePlayerTwo] + "-" + scores[scorePlayerOne];
+    }
+
 
 }
